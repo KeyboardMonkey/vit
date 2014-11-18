fbbe003edc02f7bfc8812429c2da7b2b3765ff55
Abdul Rehman <ar_malik18@yahoo.com>
2014-11-18 05:35:17 +0500

    Home, Profile
    .Home and Profile Screens are updated. (Color tags are added for
    individual course points)
    .Useless icons are deleted from graphics folder.

--- application/views/pages/profile.php
+++ application/views/pages/profile.php
@@ -22,36 +22,39 @@
 			<section class="course-cols">
 				<section class="col1">
 					<ul class="cols">
-						<li><strong>1,641</strong><br/>HTML</li>
-						<li><strong>180</strong><br/>CSS</li>
-						<li><strong>7</strong><br/>C++</li>
+						<li class="html"><strong>1,641</strong><br/>HTML</li>
+						<li class="php"><strong>937</strong><br/>PHP</li>
+						<li class="asp"><strong>831</strong><br/>ASP.Net</li>
+						<li class="java"><strong>0</strong><br/>Java</li>
 					</ul>
 				</section>
 				<section class="col2">
 					<ul class="cols">
-						<li><strong>937</strong><br/>PHP</li>
-						<li><strong>91</strong><br/>JavaScript</li>
-						<li><strong>0</strong><br/>jQuery</li>
+						<li class="android"><strong>18</strong><br/>Android</li>
+						<li class="javascript"><strong>91</strong><br/>JavaScript</li>
+						<li class="jquery"><strong>0</strong><br/>jQuery</li>
+						<li class="css"><strong>180</strong><br/>CSS</li>
 					</ul>
 				</section>
 				<section class="col3">
 					<ul class="cols">
-						<li><strong>831</strong><br/>ASP.Net</li>
-						<li><strong>18</strong><br/>Android</li>
-						<li><strong>0</strong><br/>Java</li>
+						<li class="cpp"><strong>7</strong><br/>C++</li>
+						<li class="wordpress"><strong>193</strong><br/>WordPress</li>
+						<li class="d-tools"><strong>7</strong><br/>Dev. Tools</li>
+						<li class="business"><strong>0</strong><br/>Business</li>
 					</ul>
 				</section>
 				<section class="col4">
 					<ul class="cols">
-						<li><strong>193</strong><br/>WordPress</li>
-						<li><strong>7</strong><br/>Development Tools</li>
-						<li><strong>0</strong><br/>Business</li>
+						<li class="design"><strong>10</strong><br/>Design</li>
+						<li class="other"><strong>10</strong><br/>Others</li>
 					</ul>
 				</section>
 				<section class="clear"></section>
 			</section>
 			<section class="line-border"></section>
 			<section class="info">
+				<img src="<?=base_url();?>assets/graphics/info.png" width="50px" height="50px" alt="info" />
 				<p>Points are earned whenever you take an important action on Vitorials.
 				<br/><a href="">Learn more</a> about when and how points are earned.</p>
 			</section>
