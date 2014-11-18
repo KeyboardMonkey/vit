08637f5f3eb878390f3c0905a073f37b2569e9e2
Abdul Rehman <ar_malik18@yahoo.com>
2014-11-18 23:05:33 +0500

    privacy-settings, upcoming-courses
    .privacy-settings screen is created. (side-navs to be added)
    .upcoming-courses screen is created. (side-navs to be added)

---
+++ application/views/pages/upcoming-courses.php
@@ -0,0 +1,32 @@
+<section class="container">
+	<section class="content-md">
+		<section id="roadmap">
+			<h2>Roadmap</h2>
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+		</section>
+	</section>
+	<section class="content-md">
+		
+		<section class="upcoming-courses">
+			<h3>Latest Web Trends</h3>
+			<section class="color-tag">
+				<a href="#" class="html">HTML</a>
+			</section>
+			<p class="developed-by">By Udemy</p>
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+			<button class="default" type="submit">Available Now</button>
+		</section>
+
+		<section class="upcoming-courses">
+			<h3>Built a Photo Browser Android App</h3>
+			<section class="color-tag">
+				<a href="#" class="android">Android</a>
+			</section>
+			<p class="developed-by">By Lynda</p>
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+			<h4>Coming December 21, 2014</h4>
+		</section>
+
+	</section>
+</section>
