5b48f2f1dad324a19dd506a2ac32ca17820f5dfd
vitorials <fuckyoubitch@mailinator.com>
2014-11-20 01:31:01 +0500

    Leader board
    done

--- application/views/pages/library.php
+++ application/views/pages/library.php
@@ -74,29 +74,74 @@
             <?php 
                 }
             ?>
-		
+            <?php
+            $leaders = array(new user(), new user(), new user());
+            $cat = new course_cat();
+                             
+            if($filter_type == 'category')
+                    {
+                     $cat -> load($filter_value);
+                     $leaders = $this -> user -> getLeaders($filter_value);
+                    // $leaders = $cat ->getLeaders();
+                     //print_r($cat ->getLeaders());
+                     
+                    }
+                    else{
+                        $leaders = $this -> user -> getLeaders();
+                    }
+            ?>
         <section class="clear"></section>
         <section class="top-students">
 			<h3>Top 3 Students</h3>
 			<section class="first-trophy">
 				<section class="content">
 					<img src="http://localhost/vit/assets/graphics/trophy1.svg" width="100px" height="111px">
-					<a href="">Kashif Nadeem</a>
-					<p>3025 Points</p>
+					<a href=""><?=$leaders[0] -> username;?></a>
+					<p>
+                                               <?php
+                                                if(isset($filter_type))
+                                                {
+                                                    echo $cat->getPointsEarnedInCategory($leaders[0] -> user_id) ;
+                                                }
+                                                else
+                                                {
+                                                      echo $leaders[0] -> getAllPoints($leaders[0] -> user_id);
+                                                }
+                                               ?>
+                                           Points
+                                        </p>
 				</section>
 			</section>
 			<section class="second-trophy">
 				<section class="content">
 					<img src="http://localhost/vit/assets/graphics/trophy2.svg" width="100px" height="111px">
-					<a href="">Qamar Abbasi</a>
-					<p>2905 Points</p>
+					<a href=""><?=$leaders[1] -> username;?></a>
+					<p><?php
+                                                if(isset($filter_type))
+                                                {
+                                                    echo $cat->getPointsEarnedInCategory($leaders[1] -> user_id) ;
+                                                }
+                                                else
+                                                {
+                                                      echo $leaders[1] -> getAllPoints($leaders[1] -> user_id);
+                                                }
+                                               ?> Points</p>
 				</section>
 			</section>
 			<section class="third-trophy">
 				<section class="content">
 					<img src="http://localhost/vit/assets/graphics/trophy3.svg" width="100px" height="111px">
-					<a href="">Shehroz Malik</a>
-					<p>2900 Points</p>
+					<a href=""><?=$leaders[2] -> username;?></a>
+					<p><?php
+                                                if(isset($filter_type))
+                                                {
+                                                    echo $cat->getPointsEarnedInCategory($leaders[2] -> user_id) ;
+                                                }
+                                                else
+                                                {
+                                                      echo $leaders[2] -> getAllPoints($leaders[2] -> user_id);
+                                                }
+                                               ?> Points</p>
 				</section>
 			</section>
 		</section>        
