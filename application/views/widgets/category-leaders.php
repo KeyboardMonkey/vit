28d9a7b5d94a2a2ee14daab82fd42203b688af1e
Abdul Rehman <ar_malik18@yahoo.com>
2014-11-21 00:17:17 +0500

    top-three-students widget
    updated

--- application/views/widgets/category-leaders.php
+++ application/views/widgets/category-leaders.php
@@ -1,20 +1,17 @@
+<!--
 <?php
-
-            $leaders = array(new user(), new user(), new user());
-            $cat = new course_cat();
-                             
-            if($filter_type == 'category')
-                    {
-                     $cat -> load($filter_value);
-                     $leaders = $this -> user -> getLeaders($filter_value);
-                    // $leaders = $cat ->getLeaders();
-                     //print_r($cat ->getLeaders());
-                     
-                    }
-                    else{
-                        $leaders = $this -> user -> getLeaders();
-                    }
-            ?>
+  $leaders = array(new user(), new user(), new user());
+  $cat = new course_cat();
+  if($filter_type == 'category') {
+      $cat -> load($filter_value);
+      $leaders = $this -> user -> getLeaders($filter_value);
+      // $leaders = $cat ->getLeaders();
+      //print_r($cat ->getLeaders());
+  }
+  else {
+    $leaders = $this -> user -> getLeaders();
+  }
+?>
 <section class="top-students">
 			<h3>Top 3 Students</h3>
 			<section class="first-trophy">
@@ -22,35 +19,33 @@
 					<img src="http://localhost/vit/assets/graphics/trophy1.svg" width="100px" height="111px">
 					<a href=""><?=$leaders[0]->first_name." ".$leaders[0]->last_name;?></a>
 					<p>
-                                               <?php
-                                                if(isset($filter_type))
-                                                {
-                                                    echo $cat->getPointsEarnedInCategory($leaders[0] -> user_id) ;
-                                                }
-                                                else
-                                                {
-                                                      echo $leaders[0] -> getAllPoints($leaders[0] -> user_id);
-                                                }
-                                               ?>
-                                           Points
-                                        </p>
+            <?php
+              if(isset($filter_type)) {
+                echo $cat->getPointsEarnedInCategory($leaders[0] -> user_id) ;
+              }
+              else {
+                echo $leaders[0] -> getAllPoints($leaders[0] -> user_id);
+                }
+            ?> Points
+          </p>
 				</section>
 			</section>
 			<section class="second-trophy">
 				<section class="content">
 					<img src="http://localhost/vit/assets/graphics/trophy2.svg" width="100px" height="111px">
 					<a href=""><?=$leaders[1]->first_name." ".$leaders[1]->last_name;?></a>
-					<p><?php
-                                                if(isset($filter_type))
-                                                {
-                                                    echo $cat->getPointsEarnedInCategory($leaders[1] -> user_id) ;
-                                                }
-                                                else
-                                                {
-                                                      echo $leaders[1] -> getAllPoints($leaders[1] -> user_id);
-                                                }
-                                               ?> Points</p>
+					<p>
+            <?php
+              if(isset($filter_type)) {
+                echo $cat->getPointsEarnedInCategory($leaders[1] -> user_id) ;
+              }
+              else {
+                echo $leaders[1] -> getAllPoints($leaders[1] -> user_id);
+                }
+            ?> Points
+          </p>
 				</section>
+-->
 <?php
   $leaders = array(new user(), new user(), new user());
   $cat = new course_cat();                 
