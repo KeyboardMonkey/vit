ae10b155843bcc91764d25a7de600fe1065eb0be
theunusualsuspect <ishaqzafar@gmail.com>
2014-11-22 18:00:59 +0500

    upcoming courses
    .controller, view made
    .model, table not made yet

---
+++ application/controllers/upcoming.php
@@ -0,0 +1,22 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Upcoming extends MY_Controller 
+{
+
+
+
+	public function index()
+	{
+           
+		$this->load->view('templates/header');
+		$this->load->view('templates/breadcrumbs');
+	 	$this->load->view('templates/side-navs');
+		$this->load->view('pages/upcoming-courses');
+		$this->load->view('templates/footer');
+
+	}
+
+
+
+}/*class Upcoming extends MY_Controller */
+/*end of file: upcoming.php*/
