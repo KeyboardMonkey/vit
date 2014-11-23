a5947cb1eeaa1a8eec1f647652a1f79a51611dc3
vitorials <fuckyoubitch@mailinator.com>
2014-11-23 22:13:10 +0500

    ergerg
    gergerg

--- application/models/course_review.php
+++ application/models/course_review.php
@@ -11,7 +11,27 @@ class course_review extends MY_Model{
     public $course_id;
     public $review;
     public $submittion_date;
-    
+    public $marked_by_teacher;
+    public $marks;
+
+    public function getPendingReviewsCourses()
+    {
+        $courses = array();
+        $reviews = $this-> course_review -> getWithCondition(array('marked_by_teacher' => 0));
+        foreach ($reviews as $review) {
+            $course = new course();
+            $course -> load($review -> course_id);
+            $courses[$course->course_id] =$course;
+        }
+    return $courses;
+    }
+    public function getUnmarkedReviews($_course_id = NULL, $_user_id = NULL)
+    {
+        if($_user_id == NULL) $_user_id = $this -> session -> userdata('user_id');
+        if($_course_id == NULL) $_course_id = $this -> course_id;
+        $reviews = $this-> course_review -> getWithCondition(array('marked_by_teacher' => 0, 'course_id' => $_course_id));
+        return $reviews;
+    }
     public function reviewSubmittedByUser( $_course_id = NULL,$_user_id = NULL)
     {
         if($_user_id == NULL) $_user_id = $this -> session -> userdata('user_id');
